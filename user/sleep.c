365a73f8407a0df06823f43cd080e9944e75d722
chanfun-ren <2197019485@qq.com>
2023-07-30 14:30:54 +0800

    code style

--- user/sleep.c
+++ user/sleep.c
@@ -1,8 +1,7 @@
 #include "kernel/types.h"
 #include "user/user.h"
 
-int
-main(int argc, char *argv[])
+int main(int argc, char *argv[])
 {
   if (argc <= 1) {
     fprintf(2, "usage: sleep `time`...\n");
